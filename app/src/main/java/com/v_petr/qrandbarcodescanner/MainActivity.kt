1a3f6f85e45d2d8c053b238b2175874b7ca43d14
Volodymyr Petrovskyi <volodymyr.petrovskyi@gmail.com>
2023-07-26 22:41:52 +0300

    create 3 fragments

--- app/src/main/java/com/v_petr/qrandbarcodescanner/MainActivity.kt
+++ app/src/main/java/com/v_petr/qrandbarcodescanner/MainActivity.kt
@@ -2,10 +2,13 @@ package com.v_petr.qrandbarcodescanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationBarView
 import com.v_petr.qrandbarcodescanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -13,5 +16,26 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(root)
 
+        val scannerFragment = ScannerFragment()
+        val lastScannedItemsFragment = LastScannedItemsFragment()
+        val profileFragment = ProfileFragment()
+
+        setCurrentFragment(scannerFragment)
+        val bottomNavigationView = binding.bottomNavigationView
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.scanner -> setCurrentFragment(scannerFragment)
+                R.id.list -> setCurrentFragment(lastScannedItemsFragment)
+                R.id.profile -> setCurrentFragment(profileFragment)
+            }
+            true
+        }
+
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(binding.flFragment.id, fragment)
+            commit()
+        }
 }
