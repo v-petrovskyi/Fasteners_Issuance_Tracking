8a51b0c6d1292af79f5ae8a53f2408d644615d95
Volodymyr Petrovskyi <volodymyr.petrovskyi@gmail.com>
2023-09-03 10:34:37 +0300

    fix variable has to be public

--- app/src/main/java/com/v_petr/qrandbarcodescanner/FastenerIssuanceLog.kt
+++ app/src/main/java/com/v_petr/qrandbarcodescanner/FastenerIssuanceLog.kt
@@ -1,13 +1,12 @@
 package com.v_petr.qrandbarcodescanner
 
 import java.time.LocalDateTime
-import kotlin.properties.Delegates
 
 class FastenerIssuanceLog {
-    private var partBarcode: String
-    private var materialCode: String
-    private var qty by Delegates.notNull<Int>()
-    private var dateTime: String
+    var partBarcode: String
+    var materialCode: String
+    var qty: Int
+    var dateTime: String
 
     constructor(
         partBarcode: String,
