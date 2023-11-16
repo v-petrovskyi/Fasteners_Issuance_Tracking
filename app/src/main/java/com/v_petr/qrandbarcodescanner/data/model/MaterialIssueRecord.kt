af89103041e43e5a5b76c1b40ca757543e11628d
Volodymyr Petrovskyi <volodymyr.petrovskyi@gmail.com>
2023-11-16 21:24:49 +0200

    add default val to fields

--- app/src/main/java/com/v_petr/qrandbarcodescanner/data/model/MaterialIssueRecord.kt
+++ app/src/main/java/com/v_petr/qrandbarcodescanner/data/model/MaterialIssueRecord.kt
@@ -1,7 +1,7 @@
 package com.v_petr.qrandbarcodescanner.data.model
 
 data class MaterialIssueRecord(
-    val part: String,
-    val material: String,
-    val qry: Int
+    val part: String = "",
+    val material: String = "",
+    val qry: Int = 0,
 )
