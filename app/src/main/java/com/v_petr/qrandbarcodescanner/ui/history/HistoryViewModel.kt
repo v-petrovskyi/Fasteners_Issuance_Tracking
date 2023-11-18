28eedb91f2a5750bde610510d73138bc7370e283
Volodymyr Petrovskyi <volodymyr.petrovskyi@gmail.com>
2023-11-18 21:54:54 +0200

    add scanner to fragment ScannerFragment

--- app/src/main/java/com/v_petr/qrandbarcodescanner/ui/history/HistoryViewModel.kt
+++ app/src/main/java/com/v_petr/qrandbarcodescanner/ui/history/HistoryViewModel.kt
@@ -33,4 +33,8 @@ class HistoryViewModel @Inject constructor(
             _addRecord.value = it
         }
     }
+
+    companion object {
+        const val TAG = "HistoryViewModel"
+    }
 }
--- app/src/main/java/com/v_petr/qrandbarcodescanner/ui/scanner/ScannerFragment.kt
+++ app/src/main/java/com/v_petr/qrandbarcodescanner/ui/scanner/ScannerFragment.kt
@@ -1,11 +1,15 @@
 package com.v_petr.qrandbarcodescanner.ui.scanner
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
+import com.v_petr.qrandbarcodescanner.CaptureAct
 import com.v_petr.qrandbarcodescanner.databinding.FragmentScannerBinding
 import com.v_petr.qrandbarcodescanner.ui.history.HistoryViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,7 +30,7 @@ class ScannerFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         _binding = FragmentScannerBinding.inflate(layoutInflater)
         return binding.root
@@ -42,5 +46,32 @@ class ScannerFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         // TODO: Use the ViewModel
         viewModel.add()
+        binding.buttonScanMaterial.setOnClickListener {
+            materialScan.launch(scanQrCode())
+        }
+        binding.buttonScanPart.setOnClickListener{
+            partScan.launch(scanQrCode())
+        }
+    }
+
+    private fun scanQrCode() :ScanOptions {
+        val options = ScanOptions()
+        options.setPrompt("Volume up to flash on")
+        options.setBeepEnabled(true)
+//        options.setOrientationLocked(true)
+        options.captureActivity = CaptureAct::class.java
+        return options
+    }
+
+    private val partScan = registerForActivityResult(ScanContract()) {
+        if (it.contents != null) {
+            Log.d(TAG, "part: ${it.contents}")
+        }
+    }
+
+    private val materialScan = registerForActivityResult(ScanContract()) {
+        if (it.contents != null) {
+            Log.d(TAG, "material: ${it.contents}")
+        }
     }
 }
